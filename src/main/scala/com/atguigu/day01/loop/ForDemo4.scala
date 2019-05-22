d99fa334c3d576d3ff2ffeaec2c62fba0133313c
John Doe <30752740@qq.com>
2019-05-22 08:13:05 +0800

    更新

--- src/main/scala/com/atguigu/day01/loop/ForDemo4.scala
+++ src/main/scala/com/atguigu/day01/loop/ForDemo4.scala
@@ -1,11 +1,27 @@
 package com.atguigu.day01.loop
 
+import scala.util.control.Breaks.breakable
+import scala.util.control.Breaks.break
+
 /**
   * Author lzc
   * Date 2019-05-21 15:58
   */
 object ForDemo4 {
     def main(args: Array[String]): Unit = {
+        /*for (i <- 1 to 10 if i <= 5) {
+            println(i)
+        }*/
+        // 正统
+        breakable {
+            for (i <- 1 to 10) {
+                if (i == 5) break;
+                println(i)
+            }
+        }
+        
+        println("aaa")
+        
         
     }
 }
