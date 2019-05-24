724f0cae769b6a5c20733507afa05380f0a1980a
John Doe <30752740@qq.com>
2019-05-24 11:47:36 +0800

    更新

---
+++ src/main/scala/com/atguigu/day03/traitdemo/Logger.scala
@@ -0,0 +1,38 @@
+package com.atguigu.day03.traitdemo
+
+trait Logger extends Exception{
+    
+    def printError = {
+        printStackTrace()
+    }
+}
+/*trait Logger {
+    // 指定自身类型
+    //
+    this: Exception =>
+    
+    def printError = {
+        printStackTrace()
+    }
+}*/
+
+class ConsoleLogger extends Exception with Logger {
+
+}
+
+
+class A
+
+object Demo1 {
+    def main(args: Array[String]): Unit = {
+        val logger = new ConsoleLogger
+        logger.printError
+    }
+}
+
+/*
+1. trait 继承类
+
+2. 自身类型
+
+ */
