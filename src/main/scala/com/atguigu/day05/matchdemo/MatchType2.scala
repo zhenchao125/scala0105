8397ae9d1e22044cacaec49af7df5ac463a3644d
John Doe <30752740@qq.com>
2019-05-27 15:36:29 +0800

    更新

--- src/main/scala/com/atguigu/day05/matchdemo/MatchType2.scala
+++ src/main/scala/com/atguigu/day05/matchdemo/MatchType2.scala
@@ -6,9 +6,25 @@ package com.atguigu.day05.matchdemo
   */
 object MatchType2 {
     def main(args: Array[String]): Unit = {
-        val t: Any = ""
-        t match {
         
+        //        val t: Any = Array("1", "2")
+        //        val t: Any = List(1, 2, "aaa")
+        //        val t: Any =Map(1 -> "a")
+        val t: Any = ("", "a")
+        t match {
+            //            case arr: Array[Int] => println("Array[Int]")
+            //            case arr: Array[String] => println("Array[String]" + arr.mkString(", "))
+            //            case list: List[_] => println("List[_]")
+            //            case map:Map[_, _] => println("Map")
+            case t1: (_, _) => println(t1)
         }
     }
 }
+
+
+/*
+泛型擦除
+    任何泛型类, 编译成字节码之后只有一份
+
+
+ */
---
+++ src/main/scala/com/atguigu/day05/matchdemo/MathArray.scala
@@ -0,0 +1,24 @@
+package com.atguigu.day05.matchdemo
+
+/**
+  * Author lzc
+  * Date 2019-05-27 15:24
+  */
+object MathArray {
+    def main(args: Array[String]): Unit = {
+        var arr = Array(10, 20, 30, 40, 50)
+        
+        arr match {
+            //            case Array(a, b, c) => println(a, b, c)
+            //            case Array(a, 20, b) => println(a, b)
+            //            case Array(a, _*) => println(a)
+            //            case Array(a, b, _*) => println(a)
+//            case Array(a, b, v@_*) => println(v.mkString(", "))
+        }
+    }
+}
+
+/*
+匹配的数组的内容:
+ 
+ */
